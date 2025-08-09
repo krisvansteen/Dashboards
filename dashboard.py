1ac6a3b9085bacf9d5322cce6480ab8e39eaad9e
krisvansteen <kris.van.steen@belgiancycling.be>
2025-08-09 22:11:25 +0200

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -38,7 +38,7 @@ html_template = """
 <!doctype html>
 <html>
 <head>
-    <title>MQTT Data per Topic</title>
+    <title>VOORLOPIGE UITSLAG - PROVISIONAL RESULTS </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
